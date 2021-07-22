d2fe52330f727fa771ac93f9f4210041f3d1ae70
Matheus Pimentel <matheusp.eng@gmail.com>
2021-07-22 14:52:01 -0300

    Adição de comentários para melhor entendimento do código

--- ValidaData/src/ValidaData.java
+++ ValidaData/src/ValidaData.java
@@ -31,22 +31,31 @@ public class ValidaData {
             } else if (mes == 2) {
                 // Se o mês é fevereiro, é necessário saber se o ano é bissexto ou não, ou seja, se fevereiro tem 28 ou 29 dias.
                 if (dia >= 1 && dia <= 28) {
+                	// Se o dia está entre 1 e 28, a data é válida para todos os anos
                 	System.out.println("Data Válida. A data digitada foi: " + dia + "/" + mes + "/" + ano);
                 } else if (dia == 29){
+                	// Se o dia é 29, checar se o ano é bissexto
                 	if (ano % 4 == 0) {
+                		// Se o ano é uniformemente divisível por quatro, checar se o mesmo vale para cem
                 		if (ano % 100 != 0) {
+                			// Se o ano não for uniformemente divisível por 100, ele é bissexto
                 			System.out.println("Data Válida. A data digitada foi: " + dia + "/" + mes + "/" + ano);
                 		} else {
+                			// Caso contrário, checar se ele é divisível por 400
                 			if (ano % 400 == 0) {
+                				// Se o ano é divisível por 100 e 400 ao mesmo tempo, ele é bissexto
                 				System.out.println("Data Válida. A data digitada foi: " + dia + "/" + mes + "/" + ano);
                 			} else {
+                				// Caso contrário, a data é inválida
                 				System.out.println("Data Inválida");
                 			}
                 		}
                 	} else {
+                		// Caso não seja uniformemente divisível por 4
                 		System.out.println("Data Inválida");
                 	}
                 } else {
+                	// Caso o dia seja 0 ou maior que 29
                 	System.out.println("Data Inválida");
                 }
             }
