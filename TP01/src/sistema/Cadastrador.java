78a117a855f3d422221a0743e0fc5a41806a9e74
Matheus Pimentel <matheusp.eng@gmail.com>
2021-07-29 17:19:31 -0300

    Finalização do TP01

--- TP01/src/sistema/Cadastrador.java
+++ TP01/src/sistema/Cadastrador.java
@@ -8,21 +8,112 @@ public class Cadastrador {
         Scanner ler = new Scanner(System.in);
         //Declaração de variáveis
         int opcao;
-        int numeroClientes, numeroProdutos;
         //pré cadastro do primeiro cliente
-        Cliente[] dados = new Cliente[1];
+        Cliente[] dados = new Cliente[10];
+        Cliente[] tempCliente;
         dados[0] = new Cliente();
         dados[0].nome = "Diego Silva";
         dados[0].endereco = "Rua das Palmeiras, Casa 90";
-        dados[0].telefone = "61 999999999";
+        dados[0].telefone = "999999999";
+        dados[1] = new Cliente();
+        dados[1].nome = "Anderson Cristiano";
+        dados[1].endereco = "Rua da liberdade, Casa 12";
+        dados[1].telefone = "911111111";
+        dados[2] = new Cliente();
+        dados[2].nome = "Manuela Gouveia";
+        dados[2].endereco = "Avenida são josé, Casa 1";
+        dados[2].telefone = "922222222";
+        dados[3] = new Cliente();
+        dados[3].nome = "Catarina Ferreira";
+        dados[3].endereco = "Rua das Palmeiras, Casa 4";
+        dados[3].telefone = "933333333";
+        dados[4] = new Cliente();
+        dados[4].nome = "Douglas Leal";
+        dados[4].endereco = "Rua do cacique, Casa 78";
+        dados[4].telefone = "944444444";
+        dados[5] = new Cliente();
+        dados[5].nome = "Gabriela Costa";
+        dados[5].endereco = "Setor habitacional oeste, Casa 40";
+        dados[5].telefone = "955555555";
+        dados[6] = new Cliente();
+        dados[6].nome = "Giovana das dores";
+        dados[6].endereco = "Vila recife, Casa 77";
+        dados[6].telefone = "966666666";
+        dados[7] = new Cliente();
+        dados[7].nome = "Lucrecia Navarro";
+        dados[7].endereco = "Vila da jiboia, Casa 8";
+        dados[7].telefone = "977777777";
+        dados[8] = new Cliente();
+        dados[8].nome = "Rafaela Freitas";
+        dados[8].endereco = "Rua da aliança, Casa 2";
+        dados[8].telefone = "988888888";
+        dados[9] = new Cliente();
+        dados[9].nome = "Bruno da Costa";
+        dados[9].endereco = "Rua da aliança, Casa 1";
+        dados[9].telefone = "910000000";
         //pré cadastro do primeiro produto
-        Produto[] estoque = new Produto[1];
+        Produto[] estoque = new Produto[10];
+        Produto[] tempProduto;
         estoque[0] = new Produto();
         estoque[0].nome = "Vaso de flores";
         estoque[0].descricao = "Vaso de flores com uma imagem de nossa senhora aparecida.";
         estoque[0].valor = 25.99;
         estoque[0].pctLucro = 14.3;
-        estoque[0].qtdEstoque = 4;
+        estoque[0].qtdEstoque = 18;
+        estoque[1] = new Produto();
+        estoque[1].nome = "Jarra para suco";
+        estoque[1].descricao = "Jarra roxa para suco.";
+        estoque[1].valor = 20.90;
+        estoque[1].pctLucro = 10.3;
+        estoque[1].qtdEstoque = 27;
+        estoque[2] = new Produto();
+        estoque[2].nome = "Garrafa para líquidos";
+        estoque[2].descricao = "Garrafa transparente para líquidos variados.";
+        estoque[2].valor = 37.89;
+        estoque[2].pctLucro = 16.8;
+        estoque[2].qtdEstoque = 40;
+        estoque[3] = new Produto();
+        estoque[3].nome = "Garrafa térmica";
+        estoque[3].descricao = "Garrafa para líquidos quentes ou frios.";
+        estoque[3].valor = 31.00;
+        estoque[3].pctLucro = 15.0;
+        estoque[3].qtdEstoque = 0;
+        estoque[4] = new Produto();
+        estoque[4].nome = "Caneca";
+        estoque[4].descricao = "Caneca personalizada FRIENDS.";
+        estoque[4].valor = 45.90;
+        estoque[4].pctLucro = 20.0;
+        estoque[4].qtdEstoque = 3;
+        estoque[5] = new Produto();
+        estoque[5].nome = "Copo de cerveja";
+        estoque[5].descricao = "Copo de cerveja personalizado How I Met Your Mother.";
+        estoque[5].valor = 45.90;
+        estoque[5].pctLucro = 14.0;
+        estoque[5].qtdEstoque = 3;
+        estoque[6] = new Produto();
+        estoque[6].nome = "Pote";
+        estoque[6].descricao = "Pote de geleia vermelho pintado à mão.";
+        estoque[6].valor = 14.87;
+        estoque[6].pctLucro = 9.94;
+        estoque[6].qtdEstoque = 1;
+        estoque[7] = new Produto();
+        estoque[7].nome = "Vasilhame";
+        estoque[7].descricao = "Vasilhame de sorvete personalizado feijão.";
+        estoque[7].valor = 10.90;
+        estoque[7].pctLucro = 10.3;
+        estoque[7].qtdEstoque = 45;
+        estoque[8] = new Produto();
+        estoque[8].nome = "Regador";
+        estoque[8].descricao = "Regador simples verde.";
+        estoque[8].valor = 22.50;
+        estoque[8].pctLucro = 12.6;
+        estoque[8].qtdEstoque = 31;
+        estoque[9] = new Produto();
+        estoque[9].nome = "Segurador de vela";
+        estoque[9].descricao = "Base seguradora para velas.";
+        estoque[9].valor = 18.90;
+        estoque[9].pctLucro = 10.1;
+        estoque[9].qtdEstoque = 18;
         do{
             //Impressão do menu na tela
             System.out.print("\t========== Sistema de vendas ==========\n");
@@ -40,156 +131,32 @@ public class Cadastrador {
             switch(opcao){
                 case 1->{
                     //Cadastro de cliente
-                    System.out.print("\n\tInforme o número de clientes que deseja cadastrar: ");
-                    numeroClientes = ler.nextInt();
-                    ler.nextLine();
-                    //Criar Array de novos clientes para inserção correta dos dados
-                    Cliente[] novos;
-                    //Copiar o conteúdo do banco de dados para o Array de novos clientes
-                    novos = Arrays.copyOf(dados, (numeroClientes + dados.length));
-                    for (int i = 0; i < numeroClientes; i++){
-                        //Preenchimento do novo Array com os dados desejados
-                        novos[i + dados.length] = new Cliente();
-                        System.out.print("\n\t---------------------------------------\n");
-                        System.out.print("\tInforme o nome do " + (i+1) + "° cliente: ");
-                        novos[i + dados.length].nome = ler.nextLine();
-                        System.out.print("\tInforme o endereco do " + (i+1) + "° cliente: ");
-                        novos[i + dados.length].endereco = ler.nextLine();
-                        System.out.print("\tInforme o telefone do " + (i+1) + "° cliente: ");
-                        novos[i + dados.length].telefone = ler.nextLine();
-                        System.out.print("\t---------------------------------------\n");
-                    }
-                    System.out.print("\tPressione enter para retornar ao menu...\n");
-                    ler.nextLine();
-                    System.out.print("\t=======================================\n\n");
-                    //Copiando os novos dados gerados para o banco de dados geral
-                    dados = Arrays.copyOf(novos, novos.length);
+                    tempCliente = Cliente.Cadastro(dados);
+                    dados = Arrays.copyOf(tempCliente, tempCliente.length);
                 }
                 case 2->{
                     //Busca pelo nome do cliente
-                    String buscaCliente;
-                    System.out.print("\n\t=======================================\n\n");
-                    System.out.print("\tDigite o nome do cliente que deseja procurar: ");
-                    ler.nextLine();
-                    buscaCliente = ler.nextLine();
-                    for (int j = 0; j < dados.length; j++) {
-                        if (dados[j].nome.equals(buscaCliente)) {
-                            System.out.print("\n\tCliente encontrado.");
-                            System.out.print("\n\t========== Dados do Cliente ===========\n");
-                            System.out.println("\tNome: " + dados[j].nome + "\n");
-                            System.out.println("\tEndereço: " + dados[j].endereco + "\n");
-                            System.out.println("\tTelefone: " + dados[j].telefone);
-                            System.out.print("\t---------------------------------------\n");
-                            System.out.print("\tPressione enter para retornar ao menu...\n");
-                            ler.nextLine();
-                            break;
-                        } else if ((j == (dados.length - 1))) {
-                            System.out.print("\n\tCliente não cadastrado.\n");
-                        }
-                    }
-                    System.out.print("\t=======================================\n\n");
+                    tempCliente = Cliente.Busca(dados);
+                    dados = Arrays.copyOf(tempCliente, tempCliente.length);
                 }
                 case 3->{
                     //Cadastro de produto
-                    System.out.print("\n\tInforme o número de produtos que deseja cadastrar: ");
-                    numeroProdutos = ler.nextInt();
-                    ler.nextLine();
-                    //Criar array de novos produtos
-                    Produto[] novos;
-                    //Copiar o conteúdo dos produtos já cadastrados para o array de novos produtos
-                    novos = Arrays.copyOf(estoque, (numeroProdutos + estoque.length));
-                    for (int k = 0; k < numeroProdutos; k++){
-                        novos[k+ estoque.length] = new Produto();
-                        System.out.print("\n\t---------------------------------------\n");
-                        System.out.print("\tInforme o nome do " + (k+1) + "° produto: ");
-                        novos[k + estoque.length].nome = ler.nextLine();
-                        System.out.print("\tInforme a descrição do " + (k+1) + "° produto: ");
-                        novos[k + estoque.length].descricao = ler.nextLine();
-                        System.out.print("\tInforme o valor do " + (k+1) + "° produto: ");
-                        novos[k + estoque.length].valor = ler.nextDouble();
-                        System.out.print("\tInforme a porcentagem de lucro do " + (k+1) + "° produto: ");
-                        novos[k + estoque.length].pctLucro = ler.nextDouble();
-                        System.out.print("\tInforme a quantidade em estoque do " + (k+1) + "° produto: ");
-                        novos[k + estoque.length].qtdEstoque = ler.nextInt();
-                        ler.nextLine();
-                        System.out.print("\t---------------------------------------\n");
-                    }
-                    System.out.print("\tPressione enter para retornar ao menu...");
-                    ler.nextLine();
-                    System.out.print("\t=======================================\n\n");
-                    //Copiando os novos dados gerados para o banco de estoques geral
-                    estoque = Arrays.copyOf(novos, novos.length);
+                    tempProduto = Produto.Cadastro(estoque);
+                    estoque = Arrays.copyOf(tempProduto, tempProduto.length);
                 }
                 case 4->{
                     //Busca pelo nome do produto
-                    String buscaProduto;
-                    ler.nextLine();
-                    System.out.print("\n\t=======================================\n\n");
-                    System.out.print("\tDigite o nome do produto que deseja procurar: ");
-                    buscaProduto = ler.nextLine();
-                    for(int l = 0; l < estoque.length; l++){
-                        if(estoque[l].nome.equals(buscaProduto)){
-                            System.out.println("\n\tNome do Produto: " + estoque[l].nome);
-                            System.out.println("\n\tDescrição do Produto: " + estoque[l].descricao);
-                            System.out.println("\n\tValor do Produto: " + estoque[l].valor);
-                            System.out.println("\n\tPorcentagem de lucro do Produto: " + estoque[l].pctLucro);
-                            System.out.println("\n\tQuantidade em estoque do Produto: " + estoque[l].qtdEstoque);
-                            System.out.print("\t---------------------------------------\n");
-                            System.out.print("\tPressione enter para retornar ao menu...");
-                            ler.nextLine();
-                            break;
-                        } else if (l == (estoque.length - 1)){
-                            System.out.print("\n\tProduto não cadastrado.\n");
-                        }
-                    }
-                    System.out.print("\t=======================================\n\n");
+                    tempProduto = Produto.Busca(estoque);
+                    estoque = Arrays.copyOf(tempProduto, tempProduto.length);
                 }
                 case 5->{
                     //Registro de venda
-                    String nomeProduto;
-                    int unidades;
-                    double valorTotal;
-                    ler.nextLine();
-                    System.out.print("\n\t=======================================\n\n");
-                    System.out.print("\tDigite o nome do produto para registrar sua venda: ");
-                    nomeProduto = ler.nextLine();
-                    for(int b = 0; b < estoque.length; b++){
-                        if(estoque[b].nome.equals(nomeProduto)){
-                            System.out.println("\n\tQuantidade do produto em estoque: " + estoque[b].qtdEstoque);
-                            System.out.print("\tQuantas unidades deseja vender ? ");
-                            unidades = ler.nextInt();
-                            if(unidades > estoque[b].qtdEstoque){
-                                System.out.print("\tNão é possível vender mais unidades do que existe em estoque.\n");
-                            } else {
-                                valorTotal = unidades * estoque[b].valor;
-                                estoque[b].qtdEstoque -= unidades;
-                                System.out.println("\n\tVenda de " + unidades + " unidades registrada com sucesso.");
-                                System.out.println("\tValor total da venda: R$" + valorTotal);
-                            }
-                            break;
-                        }
-                    }
-                    ler.nextLine();
-                    System.out.print("\t---------------------------------------\n");
-                    System.out.print("\tPressione enter para retornar ao menu...");
-                    ler.nextLine();
+                    tempProduto = Produto.Venda(estoque, dados);
+                    estoque = tempProduto;
                 }
                 case 6->{
                     //Amostragem do estoque
-                    ler.nextLine();
-                    System.out.print("\n\t=======================================\n");
-                    for(int t = 0; t < estoque.length; t++){
-                        if(estoque[t].qtdEstoque > 0){
-                            System.out.println("\tNome do Produto: " + estoque[t].nome);
-                            System.out.println("\tDescrição do Produto: " + estoque[t].descricao);
-                            System.out.println("\tValor do Produto: " + estoque[t].valor);
-                            System.out.println("\tPorcentagem de lucro do Produto: " + estoque[t].pctLucro);
-                            System.out.println("\tQuantidade em estoque do Produto: " + estoque[t].qtdEstoque);
-                            System.out.print("\t---------------------------------------\n");
-                        }
-                    }
-                    System.out.print("\tPressione enter para retornar ao menu...");
-                    ler.nextLine();
+                    Produto.MostraEstoque(estoque);
                 }
                 case 0-> {
                     System.out.print("\n\tSaindo do sistema...\n");
