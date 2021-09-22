003dc54cd249b02c94359111fddaec7f9c33af43
Matheus Pimentel Leal <matheusp.eng@gmail.com>
2021-09-22 15:22:08 -0300

    Melhorias na classe Cliente

--- TP3/src/Sistema/Cliente.java
+++ TP3/src/Sistema/Cliente.java
@@ -4,66 +4,87 @@ public class Cliente extends Pessoa{
     private String cpf;
 
     //Alteração do construtor {new} de Cliente
-    public Cliente(String nome, String endereco, String telefone, String cpf){
+    public Cliente(String nome, String cpf){
+        //únicos atributos ESTRITAMENTE NECESSÁRIOS de Cliente
         this.nome = nome;
-        this.endereco = endereco;
-        this.telefone = telefone;
         this.cpf = cpf;
     }
 
     public String toString(){
         return "Nome do cliente: " +this.nome+ "\n" +
+               "CPF do cliente: " +this.cpf+ "\n" +
                "Endereco do cliente: " +this.endereco+ "\n" +
-               "Telefone do cliente: " +this.telefone+ "\n" +
-               "CPF do cliente: " +this.cpf+ "\n";
+               "Telefone do cliente: " +this.telefone+ "\n";
     }
 
+    //Completa o cadastro do cliente
     @Override
     public void Cadastrar() {
-        //Nome
-        System.out.print("Digite o nome do cliente: ");
-        String n = scan.nextLine();
-        this.setNome(n);
-        //Endereco
-        System.out.print("Digite o endereco do cliente: ");
-        String e = scan.nextLine();
-        this.setEndereco(e);
-        //Telefone
-        System.out.print("Digite o telefone do cliente: ");
-        String t = scan.nextLine();
-        this.setTelefone(t);
-        //CPF
-        System.out.print("Digite o CPF do cliente: ");
-        String c = scan.nextLine();
-        this.setCpf(c);
+        //Caso ainda existam dados nulos
+        if(this.endereco == null || this.telefone == null){
+            System.out.println("Completar o cadastro do cliente: " + this.nome);
+            System.out.println("-----------------------------------------------------");
+            //Endereco
+            if(this.endereco == null){
+                System.out.print("Digite o endereco do cliente: ");
+                String e = scan.nextLine();
+                this.setEndereco(e);
+                System.out.println("-----------------------------------------------------");
+            }
+            //Telefone
+            if(this.telefone ==  null){
+                System.out.print("Digite o telefone do cliente: ");
+                String t = scan.nextLine();
+                this.setTelefone(t);
+                System.out.println("-----------------------------------------------------");
+            }
+        //Caso contrário
+        } else {
+            System.out.println("O cliente em questão já possui um cadastro completo.");
+            System.out.println("-----------------------------------------------------");
+        }
     }
 
+    //Edita apenas os dados já cadastrados
     @Override
     public void Editar() {
         //Imprimir os dados anteriores
-        System.out.println("Dados anteriores");
+        System.out.println("Dados anteriores:");
         this.Visualizar();
+        System.out.println("-----------------------------------------------------");
         //Nome
         System.out.print("Digite o novo nome do cliente: ");
         String n = scan.nextLine();
         this.setNome(n);
         //Endereco
-        System.out.print("Digite o novo endereco do cliente: ");
-        String e = scan.nextLine();
-        this.setEndereco(e);
+        if(this.endereco != null){
+            System.out.print("Digite o novo endereco do cliente: ");
+            String e = scan.nextLine();
+            this.setEndereco(e);
+        }
         //Telefone
-        System.out.print("Digite o novo telefone do cliente: ");
-        String t = scan.nextLine();
-        this.setTelefone(t);
+        if(this.telefone != null){
+            System.out.print("Digite o novo telefone do cliente: ");
+            String t = scan.nextLine();
+            this.setTelefone(t);
+        }
         //CPF
         System.out.print("Digite o novo CPF do cliente: ");
         String c = scan.nextLine();
         this.setCpf(c);
     }
 
+    //Imprime apenas os dados cadastrados
     @Override
     public void Visualizar() {
-        System.out.println(this);
+        System.out.println("Nome do cliente: " + this.nome);
+        System.out.println("CPF do cliente: " + this.cpf);
+        if(this.endereco != null){
+            System.out.println("Endereco do cliente: " + this.endereco);
+        }
+        if(this.telefone != null){
+            System.out.println("Telefone do cliente: " + this.telefone);
+        }
     }
 
     //Getters e Setters
--- TP3/src/Sistema/Principal.java
+++ TP3/src/Sistema/Principal.java
@@ -1,9 +1,22 @@
 package Sistema;
 
-import java.util.ArrayList;
-
 public class Principal {
 
     public static void main(String[] args){
+        Cliente cliente = new Cliente("Matheus Pimentel Leal", "053.721.701-07");
+        System.out.println("Funcao Editar 1:");
+        cliente.Editar();
+        System.out.println("Funcao Visualizar 1:");
+        cliente.Visualizar();
+        System.out.println("Funcao Cadastrar 1:");
+        cliente.Cadastrar();
+        System.out.println("Funcao Editar 2:");
+        cliente.Editar();
+        System.out.println("Funcao Visualizar 2:");
+        cliente.Visualizar();
+        System.out.println("Funcao Cadastrar 2:");
+        cliente.Cadastrar();
+        System.out.println("Dados finais:");
+        System.out.println(cliente);
     }
 }
